83b04d5d9e23c725ab9a92926791fc361178552f
Magic1conch <113282184+Magic1conch@users.noreply.github.com>
2022-09-27 11:59:05 +0800

    week04

---
+++ week04/week04-4.cpp
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main()
+{
+    for(int n=0;n<4;n++){
+        printf("n²{¦b¬O:%d\n",n);
+    }
+}
---
+++ week04/week04-5.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int n=6;
+    for(int a=0;a<n;a++){
+        printf("出現n:%d\n",a);
+    }
+    for(int a=1;a<=n;a++){
+        printf("接下來n:%d\n",a);
+        }
+}
+
